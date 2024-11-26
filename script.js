e25d6de217540985d44575b2c2b3cea8818ab073
Lucaaaa05 <lucaenrique05@gmail.com>
2024-11-26 16:45:54 -0300

    Mas correciones

--- script.js
+++ script.js
@@ -1,45 +1,8 @@
-import InventarioProducto from "./InventarioProducto.js";
-import Producto from "./Producto.js";
 import Carrito from "./Carrito.js";
+import Notificaciones from "./Notificaciones.js";
 
 const carrito = new Carrito();
-
-function mostrarResumenCompra() {
-    const detalleCompra = document.getElementById("detalleCompra");
-    detalleCompra.innerHTML = "";
-
-    if (!Carrito || !Array.isArray(carrito.productos)) {
-        console.error("El carrito o su lista de productos no están definidos correctamente.");
-        detalleCompra.innerHTML = "<li class='list-group-item'>Error al cargar el carrito.</li>";
-        return;
-    }
-
-    if (carrito.productos.length === 0) {
-        detalleCompra.innerHTML = "<li class='list-group-item'>El carrito está vacío.</li>";
-        return;
-    }
-
-    carrito.productos.forEach((producto) => {
-        const item = document.createElement("li");
-        item.classList.add("list-group-item");
-        item.innerHTML = `
-            ${producto.nombre} - ${producto.cantidad} x $${producto.precio.toFixed(2)} = $${(producto.precio * producto.cantidad).toFixed(2)}
-        `;
-        detalleCompra.appendChild(item);
-    });
-
-    const totalItem = document.createElement("li");
-    totalItem.classList.add("list-group-item", "list-group-item-primary", "fw-bold");
-    totalItem.textContent = `Total: $${carrito.calcularTotal()}`;
-    detalleCompra.appendChild(totalItem);
-}
-
-const btnFinalizarCompra = document.querySelector(".checkout-button");
-btnFinalizarCompra.addEventListener("click", () => {
-    mostrarResumenCompra();
-    const modal = new bootstrap.Modal(document.getElementById("modalCompra"));
-    modal.show();
-});
+const notis = new Notificaciones(carrito);
 
 document.addEventListener("DOMContentLoaded", () => {
     carrito.actualizarCarrito();
@@ -55,6 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     }
+});
+
+const btnFinalizarCompra = document.querySelector(".checkout-button");
+btnFinalizarCompra.addEventListener("click", () => {
+    notis.mostrarResumenCompra();
+    const modal = new bootstrap.Modal(document.getElementById("modalCompra"));
+    modal.show();
 });
 
 document.getElementById("cart").addEventListener("click", () => {
